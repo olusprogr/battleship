53e944ca2dfed647f3136fbc146f7123e2cc6d7b
olusprogr <olus.main333@gmail.com>
2024-08-19 12:10:45 +0200

    test

---
+++ src/components/MainMenu-UI.js
@@ -0,0 +1,256 @@
+
+const windowsWidth = window.innerWidth
+const windowsHeight = window.innerHeight
+let mobile = false;
+
+const centerX = windowsWidth / 2;
+const centerY = windowsHeight / 2;
+let sensibility = 1;
+
+let heightOfBackground = 700;
+if (windowsWidth < 768) {
+    mobile = true;
+    heightOfBackground = 300;
+    sensibility = 0.5;
+}
+
+let isAnimationRunning = true;
+let settingsButtonCliked = false;
+let tweens;
+let delay;
+
+let background;
+
+
+export function titleText(scene) {
+    const title = scene.add.text(centerX, 200, 'War Ship', {
+        fontFamily: 'Arial Black', 
+        fontSize: Math.min(windowsWidth * 0.15, 80), 
+        color: '#ffffff',
+        stroke: '#000000', 
+        strokeThickness: 8,
+        align: 'center'
+    }).setOrigin(0.5);
+
+    title.setShadow(5, 5, '#1a1a1a', 5, true, true);
+
+    return title;
+}
+
+export function addImage(scene) {
+    background = scene.add.image(512, 484, 'war-ship');
+    return background;
+}
+
+export function madeBy(scene) {
+    return scene.add.text(centerX, 600, 'Made by: Vicente, Olivier', {
+        fontFamily: 'Arial Black', 
+        fontSize: 20, 
+        color: '#ffffff',
+        stroke: '#1a1a1a', 
+        strokeThickness: 8,
+        align: 'center'
+    }).setOrigin(0.5);
+}
+
+export function multiplayerButton(scene) {
+    const multiplayerButton = scene.add.text(centerX, 450, 'Multiplayer (Not available yet)', {
+        fontFamily: 'Arial',
+        fontSize: Math.min(windowsWidth * 0.05, 32), 
+        color: '#cccccc',
+        stroke: '#000000',
+        strokeThickness: 4
+    }).setOrigin(0.5).setInteractive();
+
+    multiplayerButton.on('pointerover', () => {
+        multiplayerButton.setStyle({ fill: '#ffffff' });
+        multiplayerButton.setScale(1.05);
+    });
+
+    multiplayerButton.on('pointerout', () => {
+        multiplayerButton.setStyle({ fill: '#cccccc' });
+        multiplayerButton.setScale(1);
+    });
+
+    multiplayerButton.on('pointerdown', () => {
+        this.scene.start('Multiplayer');
+    });
+}
+
+export function singleplayerButton(scene) {
+    const singleplayerButton = scene.add.text(centerX, 350, 'Singleplayer', {
+        fontFamily: 'Arial',
+        fontSize: Math.min(windowsWidth * 0.05, 32),
+        color: '#cccccc',
+        stroke: '#000000',
+        strokeThickness: 4
+    }).setOrigin(0.5).setInteractive();
+    // In case of adding further functionality to the button just add it here under the pointerdown events
+
+    singleplayerButton.on('pointerover', () => {
+        singleplayerButton.setStyle({ fill: '#ffffff' });
+        singleplayerButton.setScale(1.05);
+    });
+
+    singleplayerButton.on('pointerout', () => {
+        singleplayerButton.setStyle({ fill: '#cccccc' });
+        singleplayerButton.setScale(1);
+    });
+
+    singleplayerButton.on('pointerdown', () => {
+        this.scene.start('Singleplayer');
+    });
+}
+
+export function randomFloatTween(scene) {
+    function action() {
+        if (!isAnimationRunning) { return }
+
+        function getRandomOffset() {
+            let offset;
+            do {
+                offset = Phaser.Math.Between(-15, 15);
+            } while (offset >= -5 && offset <= 5);
+            return offset;
+        }
+        
+        let randomX = getRandomOffset() * sensibility;
+        let randomY = getRandomOffset() * sensibility;
+        
+    
+        tweens = scene.tweens.add({
+            targets: background,
+            x: background.x + randomX,
+            y: background.y + randomY,
+            duration: Phaser.Math.Between(600, 800),
+            yoyo: true,
+            repeat: 0,
+            ease: 'Sine.easeInOut',
+            callbackScope: scene,
+            onComplete: () => {
+                delay = this.time.delayedCall(30, randomFloatTween, [], this);
+            },
+        });
+    }
+
+    action.call(scene);
+}
+
+export function settingsButtonAndFunctionality(scene) {
+    const settingsButton = scene.add.image(windowsWidth - 50 , 50, 'settings-button')
+    .setInteractive()
+    .setScale(0.1);
+
+    settingsButton.on('pointerdown', () => {
+        if (settingsButtonCliked) { return }
+        settingsButtonCliked = true;
+        
+        const settingsWindow = scene.add.graphics();
+        settingsWindow.fillStyle(0x808080, 0.8);
+        settingsWindow.fillRoundedRect(settingsButton.x - 200, settingsButton.y + 50, 200, 300, 8);
+    
+        const closeButton = scene.add.text(settingsButton.x - 50, settingsButton.y + 55, 'Close', {
+            fontFamily: 'Arial',
+            fontSize: 15,
+            color: '#ffffff',
+            stroke: '#ff0000',
+            strokeThickness: 4
+        }).setInteractive();
+    
+        let musicIsChecked = true;
+    
+        const checkbox = scene.add.rectangle(1030, 160, 20, 20, 0xffffff)
+            .setStrokeStyle(2, 0x000000)
+            .setInteractive();
+    
+        const checkboxLabel = scene.add.text(870, 150, 'Background music', {
+            fontFamily: 'Arial',
+            fontSize: '15px',
+            color: '#ffffff'
+        });
+    
+        const checkmark = scene.add.text(1022, 150, '✓', {
+            fontFamily: 'Arial',
+            fontSize: '20px',
+            color: '#00ff00'
+        }).setVisible(musicIsChecked);
+    
+        checkbox.on('pointerdown', () => {
+            if (scene.game.music) {
+                musicIsChecked = !musicIsChecked;
+                checkmark.setVisible(musicIsChecked);
+    
+                if (musicIsChecked) {
+                    this.game.music.resume();
+                } else {
+                    this.game.music.pause();
+                }
+            }
+        });
+    
+        let animationIsChecked = true;
+    
+        const checkboxLabelAnimation = scene.add.text(870, 200, 'Animation', {
+            fontFamily: 'Arial',
+            fontSize: '15px',
+            color: '#ffffff'
+        });
+    
+        const checkboxAnimation = scene.add.rectangle(1030, 210, 20, 20, 0xffffff)
+            .setStrokeStyle(2, 0x000000)
+            .setInteractive();
+    
+        const checkmarkAnimation = scene.add.text(1022, 200, '✓', {
+            fontFamily: 'Arial',
+            fontSize: '20px',
+            color: '#00ff00'
+        }).setVisible(animationIsChecked);
+    
+        checkboxAnimation.on('pointerdown', () => {
+            isAnimationRunning = !isAnimationRunning;
+            animationIsChecked = !animationIsChecked;
+            checkmarkAnimation.setVisible(animationIsChecked);
+    
+            if (!isAnimationRunning && tweens) {
+                tweens.remove();
+                if (delay) {
+                    delay.remove();
+                    delay = null;
+                }
+            } else {
+                randomFloatTween.call(scene);
+            }
+        });
+    
+        closeButton.on('pointerdown', () => {
+            closeButton.destroy();
+            settingsWindow.destroy();
+    
+            checkboxLabel.destroy();
+            checkbox.destroy();
+            checkmark.destroy();
+    
+            checkboxLabelAnimation.destroy();
+            checkboxAnimation.destroy();
+            checkmarkAnimation.destroy();
+        });
+
+        settingsButton.on('pointerover', () => {
+            scene.tweens.add({
+                targets: settingsButton,
+                angle: 100,
+                duration: 400,
+                ease: 'Power2',
+            });
+        });
+        
+        settingsButton.on('pointerout', () => {
+            scene.tweens.add({
+                targets: settingsButton,
+                angle: 0,
+                duration: 400,
+                ease: 'Power2',
+            });
+        });
+    });
+}
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser';
+import * as components from '../components/MainMenu-UI.js'
 
 export class MainMenu extends Scene
 {
@@ -25,189 +26,31 @@ export class MainMenu extends Scene
         }
 
         // Added basic main menu items
-        let background = this.add.image(centerX, heightOfBackground, 'war-ship');
+        const background = components.addImage(this);
 
         let isAnimationRunning = true;
         let settingsButtonCliked = false;
         let tweens;
         let delay;
 
-        function randomFloatTween() {
-            if (!isAnimationRunning) { return }
+        // to fixing
+        const title = components.titleText(this);
+        const gradient = title.context.createLinearGradient(0, 0, 0, title.height);
+        gradient.addColorStop(0, '#dbd9d9');
+        gradient.addColorStop(0.5, '#adacac');
+        gradient.addColorStop(1, '#dbd9d9');
+        title.setFill(gradient);
 
-            function getRandomOffset() {
-                let offset;
-                do {
-                    offset = Phaser.Math.Between(-15, 15);
-                } while (offset >= -5 && offset <= 5);
-                return offset;
-            }
-            
-            let randomX = getRandomOffset() * sensibility;
-            let randomY = getRandomOffset() * sensibility;
-            
-        
-            tweens = this.tweens.add({
-                targets: background,
-                x: background.x + randomX,  // move randomly left or right
-                y: background.y + randomY,  // move randomly up or down
-                duration: Phaser.Math.Between(600, 800), // random duration between 1 and 2 seconds
-                yoyo: true,
-                repeat: 0,
-                ease: 'Sine.easeInOut',
-                callbackScope: this,
-                onComplete: () => {
-                    delay = this.time.delayedCall(30, randomFloatTween, [], this);
-                },
-            });
-        }
-        
-        // Start the floating animation
-        randomFloatTween.call(this);
+        title.setShadow(5, 5, '#1a1a1a', 5, true, true);
 
-        // Scale the background image down a bit to show more of it on smaller screens
+        components.randomFloatTween(this);
+
         const scale = this.windowWidth < 768 ? 0.5 : 1; // If window width is less than 768px (common mobile width), scale to 80%
         background.setScale(scale);
         background.setOrigin(0.5);
 
-        // Settings button and functionality
-        const settingsButton = this.add.image(this.windowWidth - 50 , 50, 'settings-button')
-        .setInteractive()
-        .setScale(0.1);
-        
-        settingsButton.on('pointerdown', () => {
-            if (settingsButtonCliked) { return }
-            settingsButtonCliked = true;
-            
-            const settingsWindow = this.add.graphics();
-            settingsWindow.fillStyle(0x808080, 0.8);
-            settingsWindow.fillRoundedRect(settingsButton.x - 200, settingsButton.y + 50, 200, 300, 8);
-        
-            const closeButton = this.add.text(settingsButton.x - 50, settingsButton.y + 55, 'Close', {
-                fontFamily: 'Arial',
-                fontSize: 15,
-                color: '#ffffff',
-                stroke: '#ff0000',
-                strokeThickness: 4
-            }).setInteractive();
-        
-            let musicIsChecked = true;
-        
-            const checkbox = this.add.rectangle(1030, 160, 20, 20, 0xffffff)
-                .setStrokeStyle(2, 0x000000)
-                .setInteractive();
-        
-            const checkboxLabel = this.add.text(870, 150, 'Background music', {
-                fontFamily: 'Arial',
-                fontSize: '15px',
-                color: '#ffffff'
-            });
-        
-            const checkmark = this.add.text(1022, 150, '✓', {
-                fontFamily: 'Arial',
-                fontSize: '20px',
-                color: '#00ff00'
-            }).setVisible(musicIsChecked);
-        
-            checkbox.on('pointerdown', () => {
-                if (this.game.music) {
-                    musicIsChecked = !musicIsChecked;
-                    checkmark.setVisible(musicIsChecked);
-        
-                    if (musicIsChecked) {
-                        this.game.music.resume();
-                    } else {
-                        this.game.music.pause();
-                    }
-                }
-            });
-        
-            let animationIsChecked = true;
-        
-            const checkboxLabelAnimation = this.add.text(870, 200, 'Animation', {
-                fontFamily: 'Arial',
-                fontSize: '15px',
-                color: '#ffffff'
-            });
-        
-            const checkboxAnimation = this.add.rectangle(1030, 210, 20, 20, 0xffffff)
-                .setStrokeStyle(2, 0x000000)
-                .setInteractive();
-        
-            const checkmarkAnimation = this.add.text(1022, 200, '✓', {
-                fontFamily: 'Arial',
-                fontSize: '20px',
-                color: '#00ff00'
-            }).setVisible(animationIsChecked);
-        
-            checkboxAnimation.on('pointerdown', () => {
-                isAnimationRunning = !isAnimationRunning;
-                animationIsChecked = !animationIsChecked;
-                checkmarkAnimation.setVisible(animationIsChecked);
-        
-                if (!isAnimationRunning && tweens) {
-                    tweens.remove();
-                    if (delay) {
-                        delay.remove();
-                        delay = null;
-                    }
-                } else {
-                    randomFloatTween.call(this);
-                }
-            });
-        
-            closeButton.on('pointerdown', () => {
-                closeButton.destroy();
-                settingsWindow.destroy();
-        
-                checkboxLabel.destroy();
-                checkbox.destroy();
-                checkmark.destroy();
-        
-                checkboxLabelAnimation.destroy();
-                checkboxAnimation.destroy();
-                checkmarkAnimation.destroy();
-            });
-        });
-        
-        
-        settingsButton.on('pointerover', () => {
-            this.tweens.add({
-                targets: settingsButton,
-                angle: 100,
-                duration: 400,
-                ease: 'Power2',
-            });
-        });
-        
-        settingsButton.on('pointerout', () => {
-            this.tweens.add({
-                targets: settingsButton,
-                angle: 0,
-                duration: 400,
-                ease: 'Power2',
-            });
-        });
+        components.settingsButtonAndFunctionality(this);
 
-        // Creating a more visually appealing "War Ship" title with a metallic, grungy black and white style
-        const titleText = this.add.text(centerX, 200, 'War Ship', {
-            fontFamily: 'Arial Black', 
-            fontSize: Math.min(this.windowWidth * 0.15, 80), 
-            color: '#ffffff',
-            stroke: '#000000', 
-            strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
-
-        // Adding gradient effect to the text
-        const gradient = titleText.context.createLinearGradient(0, 0, 0, titleText.height);
-        gradient.addColorStop(0, '#dbd9d9');
-        gradient.addColorStop(0.5, '#adacac');
-        gradient.addColorStop(1, '#dbd9d9');
-        titleText.setFill(gradient);
-
-        // Adding shadow to the text
-        titleText.setShadow(5, 5, '#1a1a1a', 5, true, true);
 
         const createButtonBackground = (x, y, width, height) => {
             const background = this.add.graphics();
@@ -216,64 +59,12 @@ export class MainMenu extends Scene
             return background;
         };
 
-        // Singleplayer button settings
         createButtonBackground(centerX, 350, 200, 60);
-        const singleplayerButton = this.add.text(centerX, 350, 'Singleplayer', {
-            fontFamily: 'Arial',
-            fontSize: Math.min(this.windowWidth * 0.05, 32),
-            color: '#cccccc',
-            stroke: '#000000',
-            strokeThickness: 4
-        }).setOrigin(0.5).setInteractive();
-        // In case of adding further functionality to the button just add it here under the pointerdown events
+        components.singleplayerButton(this);
 
-        singleplayerButton.on('pointerover', () => {
-            singleplayerButton.setStyle({ fill: '#ffffff' });
-            singleplayerButton.setScale(1.05);
-        });
-
-        singleplayerButton.on('pointerout', () => {
-            singleplayerButton.setStyle({ fill: '#cccccc' });
-            singleplayerButton.setScale(1);
-        });
-
-        singleplayerButton.on('pointerdown', () => {
-            this.scene.start('Singleplayer');
-        });
-
-        // Multiplayer button settings
         createButtonBackground(centerX, 450, 450, 60);
-        const multiplayerButton = this.add.text(centerX, 450, 'Multiplayer (Not available yet)', {
-            fontFamily: 'Arial',
-            fontSize: Math.min(this.windowWidth * 0.05, 32), 
-            color: '#cccccc',
-            stroke: '#000000',
-            strokeThickness: 4
-        }).setOrigin(0.5).setInteractive();
-        // In case of adding further functionality to the button just add it here under the pointerdown events
+        components.multiplayerButton(this);
 
-        multiplayerButton.on('pointerover', () => {
-            multiplayerButton.setStyle({ fill: '#ffffff' });
-            multiplayerButton.setScale(1.05);
-        });
-
-        multiplayerButton.on('pointerout', () => {
-            multiplayerButton.setStyle({ fill: '#cccccc' });
-            multiplayerButton.setScale(1);
-        });
-
-        multiplayerButton.on('pointerdown', () => {
-            this.scene.start('Multiplayer');
-        });
-
-
-        this.add.text(centerX, 600, 'Made by: Vicente, Olivier', {
-            fontFamily: 'Arial Black', 
-            fontSize: 20, 
-            color: '#ffffff',
-            stroke: '#1a1a1a', 
-            strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        components.madeBy(this);
     }
 }
