980f18d29f2926e0577107b57a7b30cea92eab8b
olusprogr <olus.main333@gmail.com>
2024-09-07 22:49:30 +0200

    fixe multiplayer click bug

--- src/components/MainMenu-UI.js
+++ src/components/MainMenu-UI.js
@@ -74,7 +74,7 @@ export function multiplayerButton(scene) {
     });
 
     multiplayerButton.on('pointerdown', () => {
-        this.scene.start('Multiplayer');
+        scene.scene.start('Multiplayer');
     });
 }
 
